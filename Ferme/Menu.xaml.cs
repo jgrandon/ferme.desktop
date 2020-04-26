ded62140c4218e8a8e342ab1666d7a263a266392
U-DESKTOP-892RRGN\jota <jota.lg19@gmail.com>
2020-04-26 15:35:52 -0400

    Revert "se agrego menu boleta provisoria"
    This reverts commit 92fab72b99f59f6874a08838daa99631c308f039.

--- Ferme/Menu.xaml.cs
+++ Ferme/Menu.xaml.cs
@@ -24,17 +24,28 @@ namespace Ferme
     /// </summary>
     public partial class Menu : MetroWindow
     {
+        private List<Clientes> users;
+
+
         public Menu()
         {
             InitializeComponent();
         }
 
-      
+        public void setUsers(List<Clientes> users)
+        {
+            this.users = users;
+        }
+
+        private void Tile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
 
         private void BtnCancelar_Click(object sender, RoutedEventArgs e)
         {
             MainWindow miMainWindows = new MainWindow();
-
+            miMainWindows.setUsers(users);
             miMainWindows.Show();
             this.Close();
         }
@@ -42,66 +53,51 @@ namespace Ferme
         private void TitleAgregarUsuario_Click(object sender, RoutedEventArgs e)
         {
             FlyUsuarioNuevo.IsOpen = true;
-            FlyProveedor.IsOpen = false;
-            FlyBoletas.IsOpen = false;
         }
 
         private async void BtnIngresar_Click(object sender, RoutedEventArgs e)
         {
-            await this.ShowMessageAsync("Exito", "Usuario Agregado");
+            Clientes newUser = new Clientes();
+
+            if ( !IsUserValid() )
+            {
+                await this.ShowMessageAsync("Error", "Debe llenar todos los datos del Usuario");
+            }
+            else if ( !IsPasswordValid() )
+            {
+                await this.ShowMessageAsync("Error", "La contraseña no coincide con su confirmacion");
+
+            }
+            else 
+            {
+                newUser.Usuario = txtNuevoUsuario.Text;
+                newUser.Contraseña = txtNuevaContraseña.Password;
+                users.Add(newUser);
+
+                await this.ShowMessageAsync("Exito", "Usuario A gregado");
+                ClearUserForm();
+            }
+
+        }
+
+        private void ClearUserForm()
+        {
             txtNuevoUsuario.Text = string.Empty;
             txtNuevaContraseña.Password = string.Empty;
             txtRepetirContraseña.Password = string.Empty;
             txtNuevoUsuario.Focus();
-
-            
         }
 
-        private void TileProveedor_Click(object sender, RoutedEventArgs e)
+        private Boolean IsUserValid()
         {
-            FlyProveedor.IsOpen = true;
-            FlyUsuarioNuevo.IsOpen = false;
-            FlyBoletas.IsOpen = false;
+            return !string.IsNullOrEmpty(txtNuevoUsuario.Text)
+                    && !string.IsNullOrEmpty(txtNuevaContraseña.Password)
+                    && !string.IsNullOrEmpty(txtRepetirContraseña.Password);
         }
 
-        private void TitleRegistroFactura_Click(object sender, RoutedEventArgs e)
+        private Boolean IsPasswordValid()
         {
-            
+            return txtNuevaContraseña.Password.Equals(txtRepetirContraseña.Password);
         }
-
-        private void TitleRegistroBoleta_Click(object sender, RoutedEventArgs e)
-        {
-            FlyBoletas.IsOpen = true;
-            FlyUsuarioNuevo.IsOpen = false;
-            FlyProveedor.IsOpen = false;
-        }
-
-        private void TitleRegistroProducto_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-
-            Ferme.usuariosDataSet usuariosDataSet = ((Ferme.usuariosDataSet)(this.FindResource("usuariosDataSet")));
-            // Cargar datos en la tabla LoginEmpleados. Puede modificar este código según sea necesario.
-            Ferme.usuariosDataSetTableAdapters.LoginEmpleadosTableAdapter usuariosDataSetLoginEmpleadosTableAdapter = new Ferme.usuariosDataSetTableAdapters.LoginEmpleadosTableAdapter();
-            usuariosDataSetLoginEmpleadosTableAdapter.Fill(usuariosDataSet.LoginEmpleados);
-            System.Windows.Data.CollectionViewSource loginEmpleadosViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("loginEmpleadosViewSource")));
-            loginEmpleadosViewSource.View.MoveCurrentToFirst();
-            // Cargar datos en la tabla Boletas. Puede modificar este código según sea necesario.
-            Ferme.usuariosDataSetTableAdapters.BoletasTableAdapter usuariosDataSetBoletasTableAdapter = new Ferme.usuariosDataSetTableAdapters.BoletasTableAdapter();
-            usuariosDataSetBoletasTableAdapter.Fill(usuariosDataSet.Boletas);
-            System.Windows.Data.CollectionViewSource boletasViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("boletasViewSource")));
-            boletasViewSource.View.MoveCurrentToFirst();
-            // Cargar datos en la tabla Vendedores. Puede modificar este código según sea necesario.
-            Ferme.usuariosDataSetTableAdapters.VendedoresTableAdapter usuariosDataSetVendedoresTableAdapter = new Ferme.usuariosDataSetTableAdapters.VendedoresTableAdapter();
-            usuariosDataSetVendedoresTableAdapter.Fill(usuariosDataSet.Vendedores);
-            System.Windows.Data.CollectionViewSource vendedoresViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("vendedoresViewSource")));
-            vendedoresViewSource.View.MoveCurrentToFirst();
-        }
-
-
     }
 }
